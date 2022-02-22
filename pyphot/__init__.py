10e088f9440613b8d202b8166b11bd1882cb1c45
feigewang <fgwang.astro@gmail.com>
2022-02-21 17:31:52 -0700

    change to 0.3.0 used for IMACS reduction of J0525

--- pyphot/__init__.py
+++ pyphot/__init__.py
@@ -9,7 +9,7 @@ that can be imported by submodules.
 import warnings
 
 # Set version
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 # Import and instantiate the logger
 from pyphot import pypmsgs
--- pyphot/_version.py
+++ pyphot/_version.py
@@ -1,2 +1,2 @@
-__version__ = '0.2.0'
-__lastupdate__ = '14Jul2021'
+__version__ = '0.3.0'
+__lastupdate__ = '21Feb2022'
